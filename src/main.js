08c42da6b5769f9fde277aaf821885de31633912
lee.oneiron <leewc@brandi.co.kr>
2020-10-27 20:53:33 +0900

    vue currency 설치 및 적용

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import 'antd-button-color/dist/css/style.css'; // or 'antd-button-color/dist/css/style.less'
+import VueCurrencyInput from 'vue-currency-input'
 
+const pluginOptions = {
+  /* see config reference */
+  globalOptions: { currency: { distractionFree: { hidecrrencySymbol: true } }, locale: 'en', precision: 0, }
+}
 
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
+Vue.use(VueCurrencyInput, pluginOptions);
 Vue.filter("makeComma", val => {
   return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 })
